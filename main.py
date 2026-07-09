182b892cb0c6c9b073b3834d913698d15c2af360
ksprihar <karanvir.prihar@gmail.com>
2026-07-09 16:46:11 -0400

    Make website ready to be deployed online.

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class Base(DeclarativeBase):
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///portfolio.db")
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
 db = SQLAlchemy(app, model_class=Base)
@@ -541,3 +541,4 @@ def admin_message_delete(message_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
