ede7dc39e1306c5421e83cf6835b3e0557fe4457
ksprihar <karanvir.prihar@gmail.com>
2026-07-09 16:51:58 -0400

    Remove the fallback sqlite database reference from flask app.

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class Base(DeclarativeBase):
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///portfolio.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 
 db = SQLAlchemy(app, model_class=Base)
@@ -540,5 +540,4 @@ def admin_message_delete(message_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+    app.run(debug=False)
